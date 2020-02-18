eb7dad58ead7bc36a1ae8465c0c5d51cd43ee80e
Michael Zeng <miczeng@microsoft.com>
2020-02-18 15:36:45 -0800

    Create BlobService with connection string.

--- src/lib/blobServiceClient.ts
+++ src/lib/blobServiceClient.ts
@@ -1,4 +1,9 @@
-import { BlobService, createBlobServiceWithSas, ExponentialRetryPolicyFilter } from 'azure-storage';
+import {
+  BlobService,
+  createBlobServiceWithSas,
+  ExponentialRetryPolicyFilter,
+  createBlobService,
+} from 'azure-storage';
 import path from 'path';
 import { URL } from 'url';
 import { promisify } from 'util';
@@ -8,15 +13,14 @@ export default class BlobServiceClient implements IBlobServiceClient {
   private getBlobToText: (container: string, blob: string) => Promise<string>;
   private createBlockBlobFromText: (container: string, blob: string, text: string) => Promise<BlobService.BlobResult>;
 
-  constructor(options: IBlobServiceShimOptions) {
-    if (!options.sas) {
-      throw new Error('sas url are required for BlobServiceShim');
+  constructor(options: IBlobServiceClientOptions) {
+    let blobService: BlobService;
+    if ('sas' in options) {
+      blobService = this.createBlobServiceWithSasUrl(options);
     }
-    const url = new URL(options.sas);
-    const serviceEndpoint = url.hostname;
-    const token = url.search;
-    this.container = path.basename(url.pathname);
-    const blobService = createBlobServiceWithSas(serviceEndpoint, token).withFilter(new ExponentialRetryPolicyFilter());
+    if ('connectionString' in options) {
+      blobService = this.createBlobServiceWithConnectionString(options);
+    }
     this.getBlobToText = promisify<string, string, string>(blobService.getBlobToText.bind(blobService));
     this.createBlockBlobFromText = promisify<string, string, string, BlobService.BlobResult>(
       blobService.createBlockBlobFromText.bind(blobService),
@@ -43,6 +47,24 @@ export default class BlobServiceClient implements IBlobServiceClient {
       throw err;
     }
   }
+
+  private createBlobServiceWithSasUrl(options: IBlobServiceClientOptionsWithSasUrl) {
+    if (!options.sas) {
+      throw new Error('sas url are required for BlobServiceClient');
+    }
+    const url = new URL(options.sas);
+    const serviceEndpoint = url.hostname;
+    const token = url.search;
+    this.container = path.basename(url.pathname);
+    return createBlobServiceWithSas(serviceEndpoint, token).withFilter(new ExponentialRetryPolicyFilter());
+  }
+
+  private createBlobServiceWithConnectionString(options: IBlobServiceClientOptionsWithConnectionString) {
+    if (!options.connectionString) {
+      throw new Error('connection string are required for BlobServiceClient');
+    }
+    return createBlobService(options.connectionString).withFilter(new ExponentialRetryPolicyFilter());
+  }
 }
 
 export interface IBlobServiceClient {
@@ -50,6 +72,12 @@ export interface IBlobServiceClient {
   createBlob(blobName: string, text: string): Promise<string>;
 }
 
-export interface IBlobServiceShimOptions {
+export type IBlobServiceClientOptions = IBlobServiceClientOptionsWithSasUrl | IBlobServiceClientOptionsWithConnectionString;
+
+interface IBlobServiceClientOptionsWithSasUrl {
   sas: string;
+}
+
+interface IBlobServiceClientOptionsWithConnectionString {
+  connectionString: string;
 }

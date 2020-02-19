15901bfd645a6a2fbfba3fe17c376c9a0b91fe56
Gene Hazan <ghazan@microsoft.com>
2020-02-19 11:13:53 -0800

    Fix typo

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import { createConnection } from 'typeorm';
 import { ormConfig } from '../ormConfig';
 import { AppRoutes } from './routes';
 
-// Ideally the continuosly running job would reside in a separate repository and run as a separate process.
+// Ideally the continuously running job would reside in a separate repository and run as a separate process.
 // We would also use queues and make the job scalable. This is an oversimplified hackathon-quality implementation of a job runner.
 import { JobRunner } from './jobs/JobRunner';
 (new JobRunner()).start();

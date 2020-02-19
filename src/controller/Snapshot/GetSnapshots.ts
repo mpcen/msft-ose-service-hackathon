9a3d0c1fa6f257afb87598d9add21f822fcea372
Gene Hazan <ghazan@microsoft.com>
2020-02-19 12:47:54 -0800

    Update logging message

--- src/controller/Snapshot/GetSnapshots.ts
+++ src/controller/Snapshot/GetSnapshots.ts
@@ -2,10 +2,10 @@ import { Request, Response } from 'express';
 import { getSnapshotService } from '../../business/SnapshotService';
 
 export const getSnapshots = async (req: Request, res: Response) => {
-    console.log(`GET /${req.params.org}/${req.params.repo}/snapshots/${req.params.id} id:`, 'req.query.params:', req.query);
+    console.log(`GET /${req.params.org}/${req.params.repo}/snapshots/${req.params.id}:`, 'req.query.params:', req.query);
     const snapshotService = await getSnapshotService();
     const { org, repo, id } = req.params;
     const snapshot = await snapshotService.getById(org, repo, parseInt(id));
-    console.log(`GET /${req.params.org}/${req.params.repo}/snapshots/${req.params.id} response:`, 'req.query.params:', JSON.stringify(req.body, null, 2));
+    console.log(`GET /${req.params.org}/${req.params.repo}/snapshots/${req.params.id} response:`, JSON.stringify(snapshot, null, 2), 'req.query.params:', req.query);
     res.status(snapshot ? 200 : 404).json(snapshot);
 };

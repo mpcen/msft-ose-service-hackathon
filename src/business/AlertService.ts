7346af30f4bcbe15fab9e3981c4c9e6187d70e07
Yang Wu <wuyang@microsoft.com>
2020-02-20 10:20:52 -0800

    Call CDS to fetch alerts information

--- src/business/AlertService.ts
+++ src/business/AlertService.ts
@@ -4,14 +4,16 @@ export default class AlertService {
     constructor(){
     }
 
-    public async getAlertFromCDS() {
-      
+    public async getAlertFromCDS(org: string, repo: string, snapshotId: number) {    
+      var componentName = "express";
+      var version = "4.0.0";
+      var type = "npm"; 
+      var forgeKey = this.ForgeToKey(type);
+      var searchRequest = this.GenerateComponentSearchRequest(type, componentName, version);
+
       var component = {
           "component": {
-              "npmjs.org": {
-                  "name": "marked",
-                  "version": "0.3.9"
-              }
+            [forgeKey]: searchRequest
           }
       };
 
@@ -22,6 +24,77 @@ export default class AlertService {
       })
 
       return response.data
+    }
+
+    public ForgeToKey(type: string) : string
+    {
+        switch (type.toLocaleLowerCase())
+        {
+            case "cargo": return "cargo";
+            case "git": return "github";
+            case "maven": return "maven.org";
+            case "npm": return "npmjs.org";
+            case "nuget": return "nuget.org";
+            case "other": return "others";
+            case "pip": return "pypi";
+            case "rubygems": return "rubygem";
+            case "go": return "go";
+        }
+    }
+
+    public GenerateComponentSearchRequest(type: string, name: string, version: string)
+    {
+        let searchRequest;
+        switch (type.toLocaleLowerCase())
+        {
+            // Does this need github as well?
+            case "maven":
+                searchRequest = 
+                {                    
+                    "Version": version,
+                    "GroupId": name.split(':')[0],
+                    "ArtifactId": name.split(':')[1]
+                };
+                break;
+
+            case "git":
+                var split = name.split('/');
+                if (split.length == 2)
+                {
+                    searchRequest =
+                    {
+                        "CommitHash": version,
+                        "Repository": split[1],
+                        "Owner": split[0]
+                    };
+                }
+                else
+                {                    
+                    searchRequest =
+                    {
+                        "name": name,
+                        "version": version,
+                    };
+                }
+                break;
+
+            case "other":
+                searchRequest =
+                {
+                    "name": name,
+                    "version": version,
+                };
+                break;
+
+            default:
+                searchRequest =
+                {
+                    "name": name,
+                    "version": version,
+                };
+        }
+
+        return searchRequest;
     }
 }
 
--- src/controller/Alert/GetAlerts.ts
+++ src/controller/Alert/GetAlerts.ts
@@ -4,28 +4,7 @@ import { getAlertService } from '../../business/AlertService';
 export const getAlerts = async (req: Request, res: Response) => {
     console.log(`GET /${req.params.org}/${req.params.repo}/alerts/${req.params.snapshotId} id:`, 'req.query.params:', req.query);
     const alertService = await getAlertService();
-    const result = await alertService.getAlertFromCDS();
-    console.log(result);
-    // TODO: fetch from DB and blob storage
-    /*
-    const responseBody = {
-        id: '1',
-        createdAt: new Date(),
-        org: req.params.org,
-        repo: req.params.repo,
-        alerts: [
-            {
-                alertId: '1',
-                title: "alert1",
-                description: "description1"
-            },
-            {
-                alertId: '2',
-                title: "alert2",
-                description: "description2"
-            }
-        ]
-    };
-    console.log(`GET /${req.params.org}/${req.params.repo}/alerts/${req.params.id} response:`, 'req.query.params:', JSON.stringify(responseBody, null, 2));
-    res.json(responseBody);*/
+    const { org, repo, snapshotId } = req.params;
+    const alerts = await alertService.getAlertFromCDS(org, repo, parseInt(snapshotId));
+    res.status(alerts ? 200 : 404).json(alerts);
 }
